6d4a580082967b26fa40b0a2d6f65bf478ede3e9
Akuttyprince <yuvarajpro213@gmail.com>
2025-05-03 16:49:41 +0530

    update typewriter strings to reflect current status

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,8 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
+          "Software Developer In Progress",
+          "Freelancer", 
           "MERN Stack Developer",
           "Open Source Contributor",
         ],
